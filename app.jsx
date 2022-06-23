d7266b8c44f937a8c97f9725ded6323c83312c80
Daniel Cardozo <villa5041@gmail.com>
2022-06-23 12:25:22 +0200

    Starting to create the webpage, first, creating a template

--- app.jsx
+++ app.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import * as Server from 'react-dom/server'
+import * as ReactDOM from 'react-dom'
 
-let Greet = () => <h1>Hello, world!</h1>
-console.log(Server.renderToString(<Greet />))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello, worladasd!</h1>);
